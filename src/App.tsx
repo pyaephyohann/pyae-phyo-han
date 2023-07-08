d16ec63a78740218b7867d8fb3b47d228edf172e
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-07-08 09:21:48 +0630

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl text-yellow-200">halo</div>
+    </div>
+  );
+}
+
+export default App;
